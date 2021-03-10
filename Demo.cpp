dc38139f86d45818325e37362f781428f024167c
MSI\supav <62160112@go.buu.ac.th>
2021-03-10 16:35:41 +0700

    lastest

--- Demo.cpp
+++ Demo.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 int main()
 {
+	int a;
+	a = 2+3;
     cout << "Hello Jenkins" << endl;
     cout << "Hi Jenkins" << endl;
+    cout << "Calculation = " << a << endl;
     cout << "Last try";
     return 0;
 }
